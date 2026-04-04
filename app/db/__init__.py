47ca09110797ba214c2e249e1e724cba3dd9e906
Lexmanthefirst <lexmanthefirst@gmail.com>
2026-04-04 12:32:12 +0100

    Updates database module public API

--- app/db/__init__.py
+++ app/db/__init__.py
@@ -1,4 +1,4 @@
 from app.db.base import Base, BaseModel, QueryBuilder
-from app.db.session import SessionLocal, engine, get_db_session
+from app.db.session import AsyncSessionLocal, engine, get_db, init_db
 
-__all__ = ["Base", "BaseModel", "QueryBuilder", "engine", "SessionLocal", "get_db_session"]
+__all__ = ["Base", "BaseModel", "QueryBuilder", "engine", "AsyncSessionLocal", "get_db", "init_db"]
